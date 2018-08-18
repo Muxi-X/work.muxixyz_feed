a3c6086faa44627456982f17365dfe563752308a
CGH233 <1113713599@qq.com>
2018-08-18 16:35:23 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,30 +1,39 @@
-#coding:utf-8
+'''coding = utf-8'''
 import unittest
-import os 
-from work_muxixyz_app import create_app, db
-from flask import current_app, url_for, jsonify
-from flask_sqlalchemy import SQLALchemy
-from ..work_muxixyz_app.models import Team,Group,User,Project,Message,Statu,File,Comment
+import os
 import random
 import json
+from flask import current_app, url_for
+from work_muxixyz_app import create_app, db
+#from ..work_muxixyz_app.models import Team, Group, User, Project, Message, Statu, File, Comment
 
-from1 = [process,files,comments,teams] 
-kind = random.ranint(1,4)
-sourceID = random.ranint(1,100)
+FROM = ['process', 'files', 'comments', 'teams']
+KIND = random.randint(1, 4)
+SOURCEID = random.randint(1, 100)
+TOKEN = 0
 
 class SampleTestCase(unittest.TestCase):
 
-    def get_api_headers(self,ifToken):
-        if ifToken is True:
+    def test_app_token(self):
+        response = self.client.post(
+            url_for('api.login', _external=True),
+            data=json.dumps({
+                "username": 'test'}),
+            headers=self.get_api_headers(False))
+        get_token = json.loads(response.data.decode('utf-8'))['token']
+        global TOKEN
+        TOKEN = get_token
+
+
+    def get_api_headers(self, iftoken):
+        if iftoken is True:
             return {
-                'token': TOKEN,
+                'token':TOKEN,
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'}
-        else:
-            return {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'}
-    
+        return {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'}
 
     def setUp(self):
         self.app = create_app(
@@ -41,32 +50,31 @@ class SampleTestCase(unittest.TestCase):
         db.create_all()
 
 
-
     def test_app_exist(self):
         self.assertFalse(current_app is None)
+
 
 
 # FEED PART
 
     def test_feed_a_new(self):
-        response=self.client.post(
-            url_for('api.signup',_external=True),
+        response = self.client.post(
+            url_for('api.signup', _external=True),
             data=json.dumps({
-                "from":FROM[kind-1],
+                "from":FROM[KIND-1],
                 "action":"",
-                "kind":kind,
-                "sourceID":sourceID}),
-                headers=self.get_api_headers(False))
-        self.assertTrue(response.status_code==200)
-
+                "kind":KIND,
+                "sourceID":SOURCEID}),
+            headers=self.get_api_headers(False))
+        self.assertTrue(response.status_code == 200)
 
 
     def test_feed_b_list(self):
-        response=self.client.get(
+        response = self.client.get(
             'http://localhost/api/v1.0/feed/list/',
             headers=self.get_api_headers(True))
-        self.assertTrue(response.status_code==200)
-        s=json.loads(response.data.decode('utf-8'))['list']
-        print (s)
+        self.assertTrue(response.status_code == 200)
+        result = json.loads(response.data.decode('utf-8'))['list']
+        print result
 
 #END
