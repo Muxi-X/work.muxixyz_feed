9bd3174bcd6fc5c606431e2367669fbd0f8cf72e
Humbertzhang <504490160@qq.com>
2018-11-01 20:48:47 +0800

    feat: user can't see feed that they don't in

--- work_muxixyz_app/api/feed.py
+++ work_muxixyz_app/api/feed.py
@@ -1,12 +1,24 @@
 from flask import jsonify
 
-from ..models import Feed
+from ..models import User, Feed, User2Project
 from . import api
 from ..decorator import login_required
 from ..page import get_rows
 
 
 PAGESIZE = 20
+NOBODY=0
+NORMAL=1
+ADMIN=3
+SUPERADMIN=7
+nodata = {
+    "dataList": [],
+    "hasNext": False,
+    "pageMax": 0,
+    "pageNum": 0,
+    "rowsNum": 0
+}
+
 
 @api.route('/feed/list/<int:page>/', methods=['GET'], endpoint="getfeedlist")
 @login_required(1)
@@ -14,48 +26,75 @@ def getfeedlist(uid,page):
     """
     在feedlist中不再筛选权限，而是在点击时的api中判定是否拥有权限
     """
-    datas = get_rows(Feed, None, None, page, PAGESIZE, reverse=True)
-    
-    if len(datas['dataList']) is 0:
-        return jsonify({
-            "dataList": [],
-            "hasNext": False,
-            "pageMax": 0,
-            "pageNum": 0,
-            "rowsNum": 0
-        })
+    user = User.filter_by(id=uid).first() or None
+    # 用户未查询到，返回空
+    if not user:
+        return jsonify(nodata),404 
+    # 用户权限为NOBODY，返回空
+    if user.role is NOBODY:
+        return jsonify(nodata),401
+    else:
+        # 查询数据
+        datas = get_rows(Feed, None, None, page, PAGESIZE, reverse=True)
+        if len(datas['dataList']) is 0:
+            return jsonify(nodata),404
 
-    kindinit = datas['dataList'][0].get("source_kindid")
-    for d in datas['dataList']:
-        if d.get("source").get("kind_id") is not kindinit:
-            kindinit = d.get("source").get("kind_id")
-            d.update({"ifsplit": True})
-        else:
-            d.update({"ifsplit": False})
-    
-    return jsonify(datas)
+        kindinit = datas['dataList'][0].get("source_kindid")
+        for d in datas['dataList']:
+            if d.get("source").get("kind_id") is not kindinit:
+                kindinit = d.get("source").get("kind_id")
+                d.update({"ifsplit": True})
+            else:
+                d.update({"ifsplit": False})
+        
+        # 为管理员，则返回所有数据
+        if user.role is ADMIN or user.role is SUPERADMIN:
+            return jsonify(datas)
+        # 为普通用户，则查询用户所在的project ids,从所有的datas中删去不在的数据，再返回
+        elif user.role is NORMAL:
+            reliations = User2Project.filter_by(user_id=uid).all()
+            pids = [r.project_id for r in reliations]
+
+            for d in datas:
+                if d.source_projectid in pids:
+                    datas.get("dataList").remove(d)
+                return jsonify(datas)
+
     
     
 @api.route('/feed/list/<int:userid>/personal/<int:page>/', methods=['GET'], endpoint="getuserfeedlist")
 @login_required(1)
 def getuserfeedlist(uid, userid, page):
-    datas = get_rows(Feed, "userid", userid, page, PAGESIZE, reverse=True)    
-    if len(datas['dataList']) is 0:
-        return jsonify({
-            "dataList": [],
-            "hasNext": False,
-            "pageMax": 0,
-            "pageNum": 0,
-            "rowsNum": 0
-        })
+    user = User.filter_by(id=uid).first() or None
+    # 用户未查询到，返回空
+    if not user:
+        return jsonify(nodata),404 
+    # 用户权限为NOBODY，返回空
+    if user.role is NOBODY:
+        return jsonify(nodata),401
+    else:
+        # 查询数据,此处以userid为筛选条件 <=============== DIFFERENCE HERE
+        datas = get_rows(Feed, "userid", userid, page, PAGESIZE, reverse=True)    
+        if len(datas['dataList']) is 0:
+            return jsonify(nodata),404
 
-    kindinit = datas['dataList'][0].get("source_kindid")
-    for d in datas['dataList']:
-        if d.get("source").get("kind_id") is not kindinit:
-            kindinit = d.get("source").get("kind_id")
-            d.update({"ifsplit": True})
-        else:
-            d.update({"ifsplit": False})
-    
-    return jsonify(datas)
+        kindinit = datas['dataList'][0].get("source_kindid")
+        for d in datas['dataList']:
+            if d.get("source").get("kind_id") is not kindinit:
+                kindinit = d.get("source").get("kind_id")
+                d.update({"ifsplit": True})
+            else:
+                d.update({"ifsplit": False})
+        
+        # 为管理员，则返回所有数据
+        if user.role is ADMIN or user.role is SUPERADMIN:
+            return jsonify(datas)
+        # 为普通用户，则查询用户所在的project ids,从所有的datas中删去不在的数据，再返回
+        elif user.role is NORMAL:
+            reliations = User2Project.filter_by(user_id=uid).all()
+            pids = [r.project_id for r in reliations]
 
+            for d in datas
+                if d.source_projectid in pids:
+                    datas.get("dataList").remove(d)
+                return jsonify(datas)
